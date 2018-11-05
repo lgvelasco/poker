43449bfd7e2a234b3745e0a1adcfb6719b3667ca
luisguillermo <guillermo.velasco98@gmail.com>
2018-11-05 23:48:17 +0100

    First Draft

--- poker.py
+++ poker.py
@@ -17,6 +17,9 @@ class Card(object):
     def __str__(self):
         return "%s%s" % (self.rank, self.suit)
 
+    def __lt__(self, other):
+        return ranks.index(self.get_rank()) < ranks.index(other.get_rank())
+
 
 class Deck(object):
     def __init__(self):
@@ -50,6 +53,11 @@ class Hand(object):
             hand += str(self.cards[i]) + " "
         return hand
 
+    def highcard(self):
+        self.cards.sort()
+        highcard = self.cards[4]
+        return highcard
+
     def is_pair(self):
         for i in range(5):
             for j in range(i+1, 5):
@@ -57,13 +65,113 @@ class Hand(object):
                     return True
         return False
 
+    def is_twopair(self):
+        pairs = 0
+        for i in range(5):
+            for j in range(i+1, 5):
+                if self.cards[i].get_rank() == self.cards[j].get_rank():
+                    pairs += 1
+        if pairs == 2:
+            return True
+        else:
+            return False
+
+    def is_three(self):
+        for i in range(5):
+            for j in range(i+1, 5):
+                for k in range(j+1, 5):
+                    if self.cards[i].get_rank() == self.cards[j].get_rank() and self.cards[j].get_rank() == self.cards[k].get_rank():
+                        return True
+        return False
+
+    def is_straight(self):
+        self.cards.sort()
+
+        for i in range(4):
+            if ranks.index(self.cards[i].get_rank()) + 1 != ranks.index(self.cards[i + 1].get_rank()):
+                return False
+        return True
+
+    def is_flush(self):
+        if self.cards[0].get_suit() == self.cards[1].get_suit() == self.cards[2].get_suit() == self.cards[3].get_suit() \
+                == self.cards[4].get_suit():
+            return True
+        return False
+
+    def is_fullhouse(self):
+        self.cards.sort()
+        if self.is_three():
+            if self.cards[2].get_rank() == self.cards[0].get_rank() and self.cards[3].get_rank() == self.cards[4].get_rank():
+                return True
+            if self.cards[2].get_rank() == self.cards[4].get_rank() and self.cards[0].get_rank() == self.cards[1].get_rank():
+                return True
+        return False
+
+    def is_four(self):
+        for i in range(5):
+            for j in range(i+1, 5):
+                for k in range(j+1, 5):
+                    for l in range(k+1, 5):
+                        if self.cards[i].get_rank() == self.cards[j].get_rank() and self.cards[j].get_rank() == \
+                                self.cards[k].get_rank() and self.cards[k].get_rank() == self.cards[l].get_rank():
+                            return True
+
+    def is_straightflush(self):
+        if self.is_straight() and self.is_flush():
+            return True
+        return False
+
+    def is_royal(self):
+        if self.is_straightflush() and self.highcard().get_rank() == "A":
+            return True
+        return False
 
 
+royal = 0
+straightflush = 0
+four = 0
+fullhouse = 0
+flush = 0
+straight = 0
+three = 0
+twopair = 0
+pair = 0
+highcard = 0
 
-new_deck = Deck()
-new_deck.shuffle()
-print(new_deck)
-hand = Hand(new_deck)
-print(hand)
+for i in range(100000):
+    new_deck = Deck()
+    new_deck.shuffle()
+    # print(new_deck)
+    hand = Hand(new_deck)
+    print(hand)
+    if hand.is_royal():
+        royal += 1
+    elif hand.is_straightflush():
+        straightflush += 1
+    elif hand.is_four():
+        four += 1
+    elif hand.is_fullhouse():
+        fullhouse += 1
+    elif hand.is_flush():
+        flush += 1
+    elif hand.is_straight():
+        straight += 1
+    elif hand.is_three():
+        three += 1
+    elif hand.is_twopair():
+        twopair += 1
+    elif hand.is_pair():
+        pair += 1
+    else:
+        highcard += 1
+print("Royal: ", royal)
+print("Straight Flush: ", straightflush)
+print("Full House: ", fullhouse)
+print("Flush: ", flush)
+print("Straight: ", straight)
+print("Three of a Kind: ", three)
+print("Two Pair: ", twopair)
+print("Pair: ", pair)
+print("High Card: ", highcard)
 
 
